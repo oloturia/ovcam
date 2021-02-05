b58b55aebadb84107fb78072f4f1b26c32eaa5bd
oloturia <oloturia@gmail.com>
2021-02-05 02:28:23 +0100

    label showing cam

--- reader/readYUV422.py
+++ reader/readYUV422.py
@@ -5,40 +5,48 @@ import math
 import time
 
 
-def readImage(port_dev,width,height):
+def readImage(port_dev,width,height,regId=0xD0,regVal=0xD0):
 	ser = serial.Serial(port_dev, 8000000)
 	ser.flushInput()
 
-	ser.write(b"*")
+	ser.write(bytes(regId))
+	ser.write(bytes(regVal))
+	
+	if regId == 0xD0 and regVal == 0xD0:
+		dataY = ser.read(size=width*height)
+		dataCbCr = ser.read(size=width*height)
+		index = 0
+		dataCb = list()
+		dataCr = list()
+		Alt = True
 
-	dataY = ser.read(size=width*height)
-	dataCbCr = ser.read(size=width*height)
-	index = 0
-	dataCb = list()
-	dataCr = list()
-	Alt = True
+		for chroma in dataCbCr:
+			if Alt:
+				dataCr.extend([chroma,chroma])
+				Alt = False
+			else:
+				dataCb.extend([chroma,chroma])
+				Alt = True
 
-	for chroma in dataCbCr:
-		if Alt:
-			dataCr.extend([chroma,chroma])
-			Alt = False
-		else:
-			dataCb.extend([chroma,chroma])
-			Alt = True
+		bitmap = list()
 
-	bitmap = list()
+		for y in range(height):
+			for x in range(width):
+				Y = dataY[index]
+				Cb = dataCb[index]
+				Cr = dataCr[index]
 
-	for y in range(height):
-		for x in range(width):
-			Y = dataY[index]
-			Cb = dataCb[index]
-			Cr = dataCr[index]
+				R = int(max(0, min(255,Y + 1.40200 * (Cr - 0x80))))
+				G = int(max(0, min(255,Y - 0.34414 * (Cb - 0x80) - 0.71414 * (Cr - 0x80))))
+				B = int(max(0, min(255,Y + 1.77200 * (Cb - 0x80))))
+				bitmap.append((R,G,B))
+				index += 1
 
-			R = int(max(0, min(255,Y + 1.40200 * (Cr - 0x80))))
-			G = int(max(0, min(255,Y - 0.34414 * (Cb - 0x80) - 0.71414 * (Cr - 0x80))))
-			B = int(max(0, min(255,Y + 1.77200 * (Cb - 0x80))))
-			bitmap.append((R,G,B))
-			index += 1
+		ser.close()
+		return bitmap
+	elif regId == 0xD0 and regVal != 0xD0:
+		value = ser.read(8)
+		return value
+		
 
-	ser.close()
-	return bitmap
+
--- reader/tkinterface.py
+++ reader/tkinterface.py
@@ -3,40 +3,46 @@ from tkinter import *
 from PIL import Image, ImageTk
 import sys
 import readYUV422
+import threading
+import logging
 
-cam_width = 240
-cam_height = 320
+logging.basicConfig(level="DEBUG", format="%(name)s - %(levelname)s - %(message)s")
+logging.debug("Starting...")
 
-if len(sys.argv) == 2:
-	port = sys.argv[1]
-else:
-	port = "/dev/ttyACM0"
+cam_width = 320  #Cam resolution
+cam_height = 240
 
+image_file = Image.new(mode="RGB", size = (cam_width, cam_height)) #Image init
 
-window = Tk()
+window = Tk()               #Tkinter init
 window.geometry("340x460")
 window.title("OV7670")
 
-cam_window = Frame(window,bd=0,height=cam_height,width=cam_width)
-cam_window.place(x=10,y=10)
-cam_window.pack(side="top")
+cam_frame = Frame(window,bd=1,height=cam_height,width=cam_width) #Frame init
+cam_frame.place(x=10,y=10)
+cam_frame.pack(side="top")
 
-cam_image = Image.new(mode="RGB", size = (cam_width, cam_height))
+cam_label = Label(cam_frame) #Label init
+cam_label.place(x=0,y=0)
 
-def refreshImage(display_width,display_height,port,image):
-	bitmap = readYUV422.readImage(port,display_width,display_height)
-	index = 0
-	for y in range(display_height):
-		for y in range(display_width):
-			image.putpixel((x, y), bitmap[index])
-			index += 1
-	window.event_generate("<<Refreshed>>")
-	return image
+if len(sys.argv) == 2:    #Checks if user selected a different port
+	port = sys.argv[1]
+else:
+	port = "/dev/ttyACM0" #Default port
 
-def refreshHandler(event):
-	cam_image = refreshImage(cam_width,cam_height,port,cam_image)
+def refreshImage(): #Threading
+	while True:
+		logging.debug("Image requested")
+		bitmap = readYUV422.readImage(port,cam_width,cam_height)
+		index = 0
+		for y in range(cam_height):
+			for x in range(cam_width):
+				image_file.putpixel((x, y), bitmap[index])
+				index += 1
+		logging.debug("Image received")
+		photo = ImageTk.PhotoImage(image_file)
+		cam_label.config(image = photo)
 
-window.bind("<<Refreshed>>", refreshHandler)
-cam_window.image = cam_image
+Refresh = threading.Thread(target=refreshImage, daemon=True).start()
 
 window.mainloop()
